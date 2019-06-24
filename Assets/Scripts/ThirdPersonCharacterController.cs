ebaff96b78769240408b2505f26913ba98196474
Scott Little <scott.little@loop.colum.edu>
2019-06-24 12:10:10 -0500

    Added Cinemachine Camera for Third Person Camera

---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxMovementSpeed;
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxisRaw("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Vertical");
+        Vector3 playerMovement = new Vector3(horizontalInput, 0f, verticalInput) * maxMovementSpeed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
