635a7f601c51b5b90fdf57aef6f744b27e877d57
Scott Little <scott.little@loop.colum.edu>
2019-09-27 14:40:12 -0500

    Added IInteractable Interface and DetectObjectInRange scripts

---
+++ Assets/Scripts/ThirdPersonController/Interaction/DetectObjectInRange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObjectInRange : MonoBehaviour
+{
+    #region Serialized Fields
+    [SerializeField]
+    [Tooltip ("This is the Capsule Collider that will " +
+        "act as a detection zone for any interactable objects within range")]
+    private CapsuleCollider detectionZone;
+    #endregion
+}
