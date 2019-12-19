2899e56ecd59a3df44f4cdf4adf22d7b667d2269
Scott Little <scott.little@loop.colum.edu>
2019-12-19 14:14:03 -0600

    Re-adding the Interaction after random merge error
    2 scripts ( the one to interact with the object and the InteractableObject script) were somehow removed, along with associated commits, after merging from the adding-interaction branch into the master

---
+++ Assets/Scripts/ThirdPersonController/Interaction/InteractableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// Purpose:
+///     This script is meant to be attached to objects that the Player can interact with.
+///     
+/// Implementation Procedure:
+///     1. Attach to the object you want to make interactable.
+///     2. Set the DisplayText field in the Editor.
+///     3. Use and Interact!
+///     
+/// </summary>
+public class InteractableObject : IInteractable
+{
+    public string DisplayText {get; private set; }
+
+    /// <summary>
+    /// Called when an InteractableObject is interacted with by the Player.
+    /// </summary>
+    public void InteractWithObject()
+    {
+        Debug.Log($"Player has interacted with {DisplayText}!");
+    }
+}
